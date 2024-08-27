a887c222229be5fb8a8610b29282dd8ceed1cccd
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-08-26 18:49:59 -0700

    Clean up code organization, add color validation

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+// Imports
 const { Triangle, Circle, Square } = require("./lib/shapes");
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-/* <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-</svg> */
+// Variables
 
-// Regex support
-// https://stackoverflow.com/a/15288645/8032508
+const validColors = JSON.parse(fs.readFileSync('colors.json'));
 
 const questions = [
   {
@@ -16,7 +15,8 @@ const questions = [
   },
   {
     name: 'textColor',
-    message: 'What should the logo text color be? (choose a name or hexadecimal value)'
+    message: 'What should the logo text color be? (choose a name or hexadecimal value)',
+    validate: validateColor
   },
   {
     name: 'shape',
@@ -30,7 +30,8 @@ const questions = [
   },
   {
     name: 'shapeColor',
-    message: 'What color should the shape be? (choose a name or hexadecimal value)'
+    message: 'What color should the shape be? (choose a name or hexadecimal value)',
+    validate: validateColor
   }
 ];
 
@@ -39,6 +40,60 @@ const shapeMap = {
   'Square': Square,
   'Triangle': Triangle
 };
+
+// Script
+
+init();
+
+// Functions
+
+function validateColor(color){
+
+  // Check if valid svg color name
+  const isValidColorName = validColors.includes(color.toLowerCase())
+  
+  // Check if valid hexadecimal value
+  let isValidHexadecimalColor = false;
+  
+  if(color[0] == '#'){
+    const regexHexadecimalPair = /^[A-Fa-f0-9]{2}$/;
+    const regexTwoCharacterSplitter = /.{1,2}/g;
+    const hexadecimalPairs = color.replace('#','').match(regexTwoCharacterSplitter);
+    const pairValidation = [];
+    console.log(hexadecimalPairs);
+
+    for(const pair of hexadecimalPairs){
+      pairValidation.push(regexHexadecimalPair.test(pair));
+    }
+
+    if(pairValidation.every(pair => pair === true) && hexadecimalPairs.length == 3){
+      isValidHexadecimalColor = true;
+    }
+  }
+
+  // Check if valid rgb value
+  let isValidRGBValue = false;
+
+  if(color.slice(0,4) == 'rgb(' && color.slice(-1) == ')'){
+
+    const digitsRaw = color.replace('rgb(','').replace(')','').split(',');
+    const digitsValidation = [];
+
+    for(const digit of digitsRaw){
+      const parsedValue = parseInt(digit);
+      if(!isNaN(parsedValue)){
+        digitsValidation.push(true);
+      }
+    }
+
+    if(digitsRaw.length == 3 && digitsValidation.every(digit => digit == true)){
+      isValidRGBValue = true;
+    }
+  }
+
+  // If is valid color name, return true
+  return isValidColorName || isValidHexadecimalColor || isValidRGBValue;
+}
 
 function validateLogoText(logoText){
   const reg = /^[A-Za-z0-9]{1,3}$/;
@@ -65,8 +120,13 @@ function writeSVGFile(answers) {
   })
 }
 
-inquirer
-.prompt(questions)
-.then((answers) => {
-  writeSVGFile(answers);
-})
+
+function init(){
+
+
+  inquirer
+  .prompt(questions)
+  .then((answers) => {
+    writeSVGFile(answers);
+  })
+}
